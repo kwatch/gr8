ceb01d8e005d7833b725d422658b514e4cf1ca0b
makoto kuwata <kwa@kuwata-lab.com>
2015-08-16 08:42:11 +0900

    ruby: define 'Gr8::EnumWrapper' class

--- test/util_test.rb
+++ test/util_test.rb
@@ -132,4 +132,41 @@ END
   end
 
 
+  topic Gr8::EnumWrapper do
+
+
+    topic '#each()' do
+
+      spec "[!hloy1] splits each line into array." do
+        enum = ["A:10:x", "B:20:y", "C:30:z"]
+        wrapper = Gr8::EnumWrapper.new(enum, /:/)
+        ok {wrapper.map {|x| x }} == [["A","10","x"], ["B","20","y"], ["C","30","z"]]
+        #
+        enum = ["A 10 x", "  B  20  y", "C\t30\tz"]
+        wrapper = Gr8::EnumWrapper.new(enum)
+        ok {wrapper.map {|x| x }} == [["A","10","x"], ["B","20","y"], ["C","30","z"]]
+      end
+
+      spec "[!c22km] chomps each lin before splitting." do
+        enum = ["A:100\n", "B:200\n", "C:300\n"]
+        wrapper = Gr8::EnumWrapper.new(enum, /:/)
+        ok {wrapper.map {|x| x }} == [["A", "100"], ["B", "200"], ["C", "300"]]
+        #
+        enum = ["A 10 x\n", "  B  20  y\n", "C\t30\tz\n"]
+        wrapper = Gr8::EnumWrapper.new(enum)
+        ok {wrapper.map {|x| x }} == [["A","10","x"], ["B","20","y"], ["C","30","z"]]
+      end
+
+      spec "[!m411f] selects column when column number specified." do
+        enum = ["A 10 x\n", "  B  20  y\n", "C\t30\tz\n"]
+        wrapper = Gr8::EnumWrapper.new(enum, nil, 2)
+        ok {wrapper.map {|x| x }} == ["10", "20", "30"]
+      end
+
+    end
+
+
+  end
+
+
 end
