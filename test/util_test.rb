79cff257303af49646dbc2731b8553e73f9ada14
makoto kuwata <kwa@kuwata-lab.com>
2015-08-15 22:58:20 +0900

    ruby: remove '$stdin.lazy#map()' singleton method

--- test/util_test.rb
+++ test/util_test.rb
@@ -127,21 +127,6 @@ END
 END
       end
 
-      spec "[!zfmcx] each item is available as self in block of map()." do
-        |app, input_data|
-        code = 'map{self.inspect}'
-        sout, _ = dummy_io(input_data) { app.run(code) }
-        ok {sout} == <<'END'
-"Haruhi  100"
-"Mikuru   80"
-"Yuki    120"
-END
-        #
-        code = 'map{split[1]}'
-        sout, _ = dummy_io(input_data) { app.run(code) }
-        ok {sout} == "100\n80\n120\n"
-      end
-
     end
 
   end
