d170b33e5673084a4fac18843962e5004dcfeca4
makoto kuwata <kwa@kuwata-lab.com>
2015-08-16 18:03:18 +0900

    ruby: register 'test/test_all.rb'

---
+++ test/test_all.rb
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+###
+### $Release: 0.0.0 $
+### $Copyright: copyright(c) 2015 kuwata-lab.com all rights reserved $
+### $License: MIT License $
+###
+
+require_relative "common.rb"
+Dir.glob(File.join(File.dirname(__FILE__), "*_test.rb")).each do |fpath|
+  require_relative File.basename(fpath)
+end
